aed19eeba688e163e07f75577f7a347190a1315d
Kendrick Drews <drewskenny@gmail.com>
2023-03-15 19:38:34 -0400

    Animations and GSAP

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,28 +1,80 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
+const group = new THREE.Group()
+// group.rotation.y = 2
+scene.add(group)
+const light = new THREE.DirectionalLight( 0xffffff, 0.35 );
+	light.position.set( 1, 1, 1 ).normalize();
+	scene.add( light );
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshLambertMaterial({color: 0xff0000})
+)
+cube1.position.set(0,1,1)
+group.add(cube1)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshLambertMaterial({color: 0x00ff00})
+)
+cube2.position.set(-2,1,1)
+group.add(cube2)
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1,1,1),
+  new THREE.MeshLambertMaterial({color: 0x0000ff})
+)
+cube3.position.set(2,1,1)
+group.add(cube3)
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+camera.position.z = 9
 scene.add(camera)
 
+
+window.onresize = function () {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+};
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+console.log(group)
+
+const rotateChildren = function () {
+  group.children.forEach((element, index) => {
+    element.rotation.x += 0.01 * (index +1)
+    
+  });
+}
+
+gsap.to(group.position, {duration: 1, delay: 1, x: 2})
+gsap.to(group.position, {duration: 1, delay: 2, x: 0})
+const clock = new THREE.Clock()
+// Animations
+const loop = () => {
+  // Will keep rotation at a constant framerate. Helps account for Hardware.
+  const elapsedTime = clock.getElapsedTime();
+  // Update Objects
+  group.rotation.y =  elapsedTime
+  rotateChildren()
+
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(loop)
+}
+
+loop()
