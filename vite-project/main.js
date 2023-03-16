190a13bc1a6e61e85d6898a380c306a81c1df417
Kendrick Drews <drewskenny@gmail.com>
2023-03-16 16:53:19 -0400

    Debug, abstractions, and camera movement

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,6 +1,32 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { fullScreen } from './src/fullscreen'
+import * as dat from 'dat.gui'
 import gsap from 'gsap'
 
+const parameters = {
+  color: 0xff0000,
+  spin: () => { 
+    gsap.to(cube1.rotation, { duration: 1, y: cube1.rotation.y + 10 })
+  }
+}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0
+}
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5
+  cursor.y = -(event.clientY / sizes.height - 0.5)
+})
 // Scene
 const scene = new THREE.Scene()
 
@@ -10,9 +36,14 @@ scene.add(group)
 const light = new THREE.DirectionalLight( 0xffffff, 0.35 );
 	light.position.set( 1, 1, 1 ).normalize();
 	scene.add( light );
+  // const light2 = new THREE.DirectionalLight( 0xff00ff, 0.45 );
+	// light2.position.set( -1, -1, -1 ).normalize();
+	// scene.add( light2 );
+  const light3 = new THREE.AmbientLight( 0x45bbcc ); // soft white light
+scene.add( light3 );
 const cube1 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshLambertMaterial({color: 0xff0000})
+  new THREE.MeshLambertMaterial({color: parameters.color})
 )
 cube1.position.set(0,1,1)
 group.add(cube1)
@@ -28,31 +59,36 @@ const cube3 = new THREE.Mesh(
 )
 cube3.position.set(2,1,1)
 group.add(cube3)
-// Sizes
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-camera.position.z = 9
+// const aspectRatio = sizes.width/sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1 ,-1, 0.1, 100)
+camera.position.z = 10
 scene.add(camera)
+// controls
 
 
-window.onresize = function () {
+window.addEventListener('resize', () => {
 
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-};
+});
+
+// DoubleClick to fullscreen
+window.addEventListener('dblclick', fullScreen)
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 console.log(group)
 
 const rotateChildren = function () {
@@ -61,20 +97,35 @@ const rotateChildren = function () {
     
   });
 }
-
-gsap.to(group.position, {duration: 1, delay: 1, x: 2})
-gsap.to(group.position, {duration: 1, delay: 2, x: 0})
+// GSAP Animation example
+// gsap.to(group.position, {duration: 1, delay: 1, x: 2})
+// gsap.to(group.position, {duration: 1, delay: 2, x: 0})
 const clock = new THREE.Clock()
 // Animations
 const loop = () => {
   // Will keep rotation at a constant framerate. Helps account for Hardware.
   const elapsedTime = clock.getElapsedTime();
   // Update Objects
-  group.rotation.y =  elapsedTime
+  // group.rotation.y =  elapsedTime
   rotateChildren()
 
+  // Update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2 ) * 5
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2 ) * 5
+  // camera.position.y = cursor.y * 5
+  // camera.lookAt(cube1.position)
+
+  // update controls
+  controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
-
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 loop()
+
+// Debug UI
+const gui = new dat.GUI();
+gui.add(cube1.position, 'y', -3, 3, 0.01)
+gui.addColor(parameters, 'color').onChange(() => { cube1.material.color.set(parameters.color); })
+gui.add(parameters, 'spin')
---
+++ vite-project/src/fullscreen.js
@@ -0,0 +1,27 @@
+export const fullScreen = () => {
+    const canvas = document.querySelector('canvas.webgl')
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullscreenElement)
+    {
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else
+    {
+        if(document.exitFullscreen)
+        {
+            document.exitFullscreen()
+        }
+        else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+}
