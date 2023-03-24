411b68ac2ee7fbce4899a3111c9729d97e000a43
Kendrick Drews <drewskenny@gmail.com>
2023-03-24 17:32:37 -0400

    Lights and Shadows

--- vite-project/main.js
+++ vite-project/main.js
@@ -33,33 +33,62 @@ const scene = new THREE.Scene()
 const group = new THREE.Group()
 // group.rotation.y = 2
 scene.add(group)
-const light = new THREE.DirectionalLight( 0xffffff, 0.35 );
-	light.position.set( 1, 1, 1 ).normalize();
-	scene.add( light );
-  // const light2 = new THREE.DirectionalLight( 0xff00ff, 0.45 );
-	// light2.position.set( -1, -1, -1 ).normalize();
-	// scene.add( light2 );
-  const light3 = new THREE.AmbientLight( 0x45bbcc ); // soft white light
-scene.add( light3 );
+// Lights
+const ambientLight = new THREE.AmbientLight()
+ambientLight.color = new THREE.Color(0xffffff)
+ambientLight.intensity = 0.5
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+directionalLight.position.set(2,2,-1)
+directionalLight.castShadow = true
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
+// Material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.4
+// const light = new THREE.DirectionalLight( 0xffffff, 0.35 );
+// 	light.position.set( 1, 1, 1 ).normalize();
+// 	scene.add( light );
+//   // const light2 = new THREE.DirectionalLight( 0xff00ff, 0.45 );
+// 	// light2.position.set( -1, -1, -1 ).normalize();
+// 	// scene.add( light2 );
+//   const light3 = new THREE.AmbientLight( 0x45bbcc ); // soft white light
+// scene.add( light3 );
+// Plane
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20,20),
+  material
+)
+floor.receiveShadow = true
+scene.add(floor)
+floor.rotation.x =  - Math.PI * 0.5
+
 const cube1 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshLambertMaterial({color: parameters.color})
+  material
 )
 cube1.position.set(0,1,1)
 group.add(cube1)
 const cube2 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshLambertMaterial({color: 0x00ff00})
+  material
 )
 cube2.position.set(-2,1,1)
 group.add(cube2)
 const cube3 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
-  new THREE.MeshLambertMaterial({color: 0x0000ff})
+  material
 )
 cube3.position.set(2,1,1)
 group.add(cube3)
 
+cube1.castShadow = true
+cube2.castShadow = true
+cube3.castShadow = true
 // Texture
 // const textureLoader = new THREE.TextureLoader()
 // const texture = textureLoader.load('/textures/door/color.jpg')
@@ -68,7 +97,8 @@ group.add(cube3)
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 // const aspectRatio = sizes.width/sizes.height
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1 ,-1, 0.1, 100)
-camera.position.z = 10
+camera.position.y = 4
+camera.position.z = 12
 scene.add(camera)
 // controls
 
@@ -93,6 +123,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 console.log(group)
+renderer.shadowMap.enabled = true
 
 const rotateChildren = function () {
   group.children.forEach((element, index) => {
