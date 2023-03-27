3cbbbc7c670fcb491a3b2b9cba9efaf1a4ae91a1
Kendrick Drews <drewskenny@gmail.com>
2023-03-27 10:44:03 -0400

    Particles and Particle Animation

--- vite-project/main.js
+++ vite-project/main.js
@@ -18,6 +18,7 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 }
+
 // Cursor
 const cursor = {
   x: 0,
@@ -27,12 +28,14 @@ window.addEventListener('mousemove', (event) => {
   cursor.x = event.clientX / sizes.width - 0.5
   cursor.y = -(event.clientY / sizes.height - 0.5)
 })
+
 // Scene
 const scene = new THREE.Scene()
 
 const group = new THREE.Group()
 // group.rotation.y = 2
 scene.add(group)
+
 // Lights
 const ambientLight = new THREE.AmbientLight()
 ambientLight.color = new THREE.Color(0xffffff)
@@ -58,6 +61,8 @@ material.roughness = 0.4
 // 	// scene.add( light2 );
 //   const light3 = new THREE.AmbientLight( 0x45bbcc ); // soft white light
 // scene.add( light3 );
+
+// Geometry
 // Plane
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20,20),
@@ -73,12 +78,14 @@ const cube1 = new THREE.Mesh(
 )
 cube1.position.set(0,1,1)
 group.add(cube1)
+
 const cube2 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
   material
 )
 cube2.position.set(-2,1,1)
 group.add(cube2)
+
 const cube3 = new THREE.Mesh(
   new THREE.BoxGeometry(1,1,1),
   material
@@ -89,6 +96,38 @@ group.add(cube3)
 cube1.castShadow = true
 cube2.castShadow = true
 cube3.castShadow = true
+
+// Particles
+// const particleGeometry = new THREE.SphereBufferGeometry(1,32,32)
+const particleGeometry = new THREE.BufferGeometry()
+
+// random buffer Geo
+const count = 5000
+
+const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
+
+for (let i = 0; i < count * 3; i++) {
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random()
+}
+
+particleGeometry.setAttribute( 'position', new THREE.BufferAttribute(positions, 3) )
+particleGeometry.setAttribute( 'color', new THREE.BufferAttribute(colors, 3) )
+
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.02,
+  sizeAttenuation: true,
+  // color: '#ff88cc',
+  transparent: true,
+  // alphaMap: particletexture,
+  alphaTest: 0.001,
+  vertexColors: true
+})
+
+const particles = new THREE.Points(particleGeometry, particlesMaterial)
+scene.add(particles)
+
 // Texture
 // const textureLoader = new THREE.TextureLoader()
 // const texture = textureLoader.load('/textures/door/color.jpg')
@@ -97,24 +136,11 @@ cube3.castShadow = true
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 // const aspectRatio = sizes.width/sizes.height
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1 ,-1, 0.1, 100)
+
 camera.position.y = 4
 camera.position.z = 12
 scene.add(camera)
-// controls
 
-
-window.addEventListener('resize', () => {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-});
-
-// DoubleClick to fullscreen
-window.addEventListener('dblclick', fullScreen)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -134,7 +160,9 @@ const rotateChildren = function () {
 // GSAP Animation example
 // gsap.to(group.position, {duration: 1, delay: 1, x: 2})
 // gsap.to(group.position, {duration: 1, delay: 2, x: 0})
+
 const clock = new THREE.Clock()
+
 // Animations
 const loop = () => {
   // Will keep rotation at a constant framerate. Helps account for Hardware.
@@ -149,14 +177,35 @@ const loop = () => {
   // camera.position.y = cursor.y * 5
   // camera.lookAt(cube1.position)
 
+  // Update Particles
+  // particles.rotation.y = elapsedTime * 0.2
+  
+
   // update controls
   controls.update()
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
+
+// Camera Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+
 loop()
+
+
+window.addEventListener('resize', () => {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+});
+
+// DoubleClick to fullscreen
+// window.addEventListener('dblclick', fullScreen)
 
 // Debug UI
 const gui = new dat.GUI();
