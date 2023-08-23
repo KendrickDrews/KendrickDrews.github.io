a11986716e1225da9c31708535199667f1772a8e
Kendrick Drews <drewskenny@gmail.com>
2023-08-22 20:37:03 -0400

    Added my own resume to portfolio

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -1,8 +1,9 @@
 import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF,  } from '@react-three/drei'
-
+import Resume from './Resume'
 export default function Experience() {
 
     const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+    const computerPosition = {x: -0.5, y: - 1.2, z: - 1.2}
 
     return <>
 
@@ -12,40 +13,47 @@ export default function Experience() {
 
         <PresentationControls 
             global 
-            rotation={ [0.13, 0.1, 0] }
+            rotation={ [0.13, 0.5, 0] }
             polar={ [ -0.4, 0.2] }
             azimuth={ [ -1, 0.75] }
-            config={ { mass: 2, tension: 400 } }
-            snap={{ mass: 4, tension: 400 } }
+            config={ { mass: 2, tension: 200 } }
+            snap={{ mass: 4, tension: 200 } }
         >
             <Float rotationIntensity={0.4}>
-
+                {/* Light from Computer */}
                 <rectAreaLight 
                     width={2.5 }
                     height={ 1.65 }
                     intensity={ 65 }
                     color={ '#ff6900' }
                     rotation={ [ 0.1, Math.PI, 0 ] }
-                    position={ [ 0, 0.55, -1.15 ] }
+                    position={ [ 0, (computerPosition.y + 0.55 ),(computerPosition.z - 1.15 ) ] }
                 />
-
+                {/* Computer */}
                 <primitive 
                     object={computer.scene} 
-                    position-y={ - 1.2 }
+                    position-x={ computerPosition.x }
+                    position-y={ computerPosition.y }
+                    position-z={ computerPosition.z }
+                    onPointerEnter={( ) => console.log('enter')} // see note 1
+                    onPointerLeave={( ) => console.log('leave')} // see note 1
                 >
                     <Html
                         transform
+                        occlude="blending"
                         wrapperClass='htmlScreen'
                         distanceFactor={ 1.17 }
                         position={ [ 0, 1.56, -1.4 ] }
                         rotation-x={ -0.256 }
+                        zIndexRange={ [100,0] }
                     > 
-                        <iframe src="https://bruno-simon.com/html/" /> 
+                        {/* <iframe src="https://bruno-simon.com/html/" />  */}
+                        <Resume />
                     </Html>
                 </primitive>
                 <Text 
                     // font=""
-                    fontSize={ 1 }
+                    fontSize={ 0.5 }
                     position={ [ 2, 0.75, 0.75 ] }
                     rotation-y={ -1.25 } 
                     maxWidth={ 2 }
@@ -53,11 +61,12 @@ export default function Experience() {
                 > Kendrick Drews </Text>
             </Float>
         </PresentationControls>
+
         <ContactShadows 
-            position-y={ - 1.4} 
-            opacity={0.4}
+            position-y={ - 1.4 } 
+            opacity={ 0.4 }
             scale={ 5 }
-            blur={2.4}
+            blur={ 5.4 }
         />
         
     </>
---
+++ portfolio/src/components/Resume.tsx
@@ -0,0 +1,62 @@
+
+
+export default function Resume() {
+
+    return <div className="resume-container">
+            <h1>Experience</h1>
+            <section>
+                <h2>NU Borders, Reston/Remote — Senior Front End Developer </h2>
+                <h3><i>Mar 2019 - present</i></h3>
+                <p> Developed React applications with Node backends for the Defense Intelligence
+Agency and United States Department of State. Responsible for Front End
+Architecture. Worked across teams to develop microservices. Produced
+data-visualizations and quarterly reports.</p>
+
+                <h2>Amida, Washington DC — UI/UX Developer </h2>
+                <h3><i>Mar 2019 - present</i></h3>
+                <p> Developed client applications with React. Conduct user interviews, user testing,
+and analytics for developmental work on VA Software. Develop front-end
+solutions for web applications. Producing wireframes, mockups, prototypes, and
+front-end code.</p>
+
+                <h2>TRI, Maryland— Front End Developer </h2>
+                <h3><i>January 2016 - March 2018</i></h3>
+                <p>Create, Edit, and/or Delete HTML and other files then publish changes to public
+website(s). Managing government webpages to provide 508 compliant and
+user-friendly experiences. Responsible for creating and delivering server-side
+code that follows company standards and procedures. Writing code and scripts to
+assist our database developers, testers, and senior developers.</p>
+
+                <h2> The INC Lab, Tysons, VA— 2D/3D Designer</h2>
+                <h3><i>January 2016 - March 2018</i></h3>
+                <p> Worked directly with clients to facilitate efficient design to development
+workflow, managed client expectations. Responsible for the creation of all visual
+assets; Marketing materials, animations, research, front-end code, data
+visualizations, wireframes, prototypes, design guides, and illustrations. Designed
+company website. https://www.theinclab.com</p>
+            </section>
+            <h1>Skills</h1>
+            <section>
+                <p> React, Node, Redux,
+Typescript, Javascript,
+Cypress, React Testing
+Library, Next.js
+CSS, SCSS, prettier, ESLint
+AWS, Docker, postman,
+Yarn, NPM, MySQL
+Confluence, Jira, GIT,
+GitLab, Jenkins
+Agile, Customer Relations,
+SCRUM Master
+Figma, Photoshop, Sketch,
+InVision, Adobe XD,
+Sharepoint</p>
+            </section>
+            <h1>Education</h1>
+            <section>
+                <h2>Virginia Commonwealth University, Richmond, VA— Bachelors of
+Fine-Arts </h2>
+                <p>2009 - 2013 </p>
+            </section>
+    </div>
+}
