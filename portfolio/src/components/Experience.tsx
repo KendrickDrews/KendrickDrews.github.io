899de391463a9454021a2c3dbac8021a35c2a7e8
Kendrick Drews <drewskenny@gmail.com>
2023-08-23 18:44:10 -0400

    Zoom and animation start

--- portfolio/src/components/Experience.tsx
+++ portfolio/src/components/Experience.tsx
@@ -1,16 +1,49 @@
-import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF,  } from '@react-three/drei'
+import React from 'react'
+// import * as THREE from 'three'
+import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF, PerspectiveCamera,  } from '@react-three/drei'
 import Resume from './Resume'
+import { useFrame, useThree } from '@react-three/fiber'
+
+
 export default function Experience() {
 
+    const [zoom, setZoom] = React.useState(false)
     const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
     const computerPosition = {x: -0.5, y: - 1.2, z: - 1.2}
+    const computerRef = React.useRef()
+
+    const cameraRef = React.useRef<any>()
+    const cameraZoom = zoom ? 1 : 8;
+
+    const cam = useThree((state) => state.camera)
+
+    function focusScreen() {
+
+        if (zoom) {
+            cam.lookAt(0,0,0);
+            
+            setZoom(!zoom);
+        }
+        else {
+            cam.lookAt(computerPosition.x, computerPosition.y, computerPosition.z)
+            setZoom(!zoom);
+        }
+        
+        console.log(computerRef)
+        
+    }
 
     return <>
 
         <Environment preset="city" />
 
         <color args={ [ '#241a1a' ] } attach="background" />
-
+        <PerspectiveCamera 
+            makeDefault 
+            ref={cameraRef}
+            position={[0,0, cameraZoom]}
+            rotation={[0,0,0]}
+        />
         <PresentationControls 
             global 
             rotation={ [0.13, 0.5, 0] }
@@ -22,6 +55,7 @@ export default function Experience() {
             <Float rotationIntensity={0.4}>
                 {/* Light from Computer */}
                 <rectAreaLight 
+                visible={true}
                     width={2.5 }
                     height={ 1.65 }
                     intensity={ 65 }
@@ -31,12 +65,12 @@ export default function Experience() {
                 />
                 {/* Computer */}
                 <primitive 
+                    ref={computerRef}
                     object={computer.scene} 
                     position-x={ computerPosition.x }
                     position-y={ computerPosition.y }
                     position-z={ computerPosition.z }
-                    onPointerEnter={( ) => console.log('enter')} // see note 1
-                    onPointerLeave={( ) => console.log('leave')} // see note 1
+                    onClick={( ) => focusScreen()} // see note 1
                 >
                     <Html
                         transform
@@ -46,8 +80,8 @@ export default function Experience() {
                         position={ [ 0, 1.56, -1.4 ] }
                         rotation-x={ -0.256 }
                         zIndexRange={ [100,0] }
+                        onClick={() => setZoom(!zoom)}
                     > 
-                        {/* <iframe src="https://bruno-simon.com/html/" />  */}
                         <Resume />
                     </Html>
                 </primitive>
