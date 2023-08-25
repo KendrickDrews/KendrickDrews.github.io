5219b79abbe4b1435daddee661464c7f36c1a2ec
Kendrick Drews <drewskenny@gmail.com>
2023-08-24 20:06:50 -0400

    Resume Formatting:Start

--- portfolio/src/components/Resume.tsx
+++ portfolio/src/components/Resume.tsx
@@ -1,62 +1,134 @@
 
+import { Col, Row } from 'antd';
 
 export default function Resume() {
 
-    return <div className="resume-container">
-            <h1>Experience</h1>
-            <section>
-                <h2>NU Borders, Reston/Remote — Senior Front End Developer </h2>
-                <h3><i>Mar 2019 - present</i></h3>
-                <p> Developed React applications with Node backends for the Defense Intelligence
-Agency and United States Department of State. Responsible for Front End
-Architecture. Worked across teams to develop microservices. Produced
-data-visualizations and quarterly reports.</p>
+    return <>
+        <div className="resume-container">
+            <Row> 
+                <Col span={8}>
+                </Col> 
+                <Col> 
+                    <Row><h1> Kendrick Drews </h1></Row>
+                    <Row><h3> Front End Developer / UI / UX </h3></Row>
+                </Col> 
+            </Row>
+            
+            <Row  align="middle" justify="center"> 
+                <Col span={8}>
+                    <h2> Contact </h2>
+                </Col> 
+                <Col span={16}> 
+                    <Row>
+                        <Col span={8}> 571 294 5260 </Col>
+                        <Col span={8}> drewskenny@gmail.com </Col>
+                        <Col span={8}> Howell, Michigan, USA </Col>
+                    </Row>
+                </Col> 
+            </Row>
+            <Row  align="middle"> 
+                <Col span={8}>
+                    <h2> Work Experience </h2>
+                </Col> 
+            </Row>
+            <Row  align="middle" justify="center"> 
+                <Col span={8}>
+                        <Row> Mar 2019 - present </Row>
+                        <Row> NU Borders </Row>
+                        <Row> Reston VA /Remote </Row>
+                </Col> 
+                <Col span={16}> 
+                        <Row > <h3>Senior Front End Developer</h3> </Row>
+                        <Row > Developed React applications with Node backends for the Defense Intelligence Agency and United States Department of State. Responsible for Front End Architecture. Worked across teams to develop microservices. Produced data-visualizations and quarterly reports. </Row>
+                        <Row > 
+                            <ul>
+                                <li> Next.js, React, Svelte, Redux, eslint, Jest, Cypress, Playwright, webpack, Parcel, TypeScript, JavaScript </li>
+                                <li>Node.js, Express, ElasticSearch, Kibana, Spark, Java/Scala/Kotlin, Rust, Docker, Kubernetes, saltstack</li>
+                                <li>AWS, EC2, Mongo, PostgresSQL, Lambda, ECS, EMR, Terraform, GitHub, BitBucket, Jenkins, blockchain</li>
+                                <li>Jira, Confluence, ZenHub, Scaled Agile, Kanban, Microservices, Microfrontends, InnerSource</li>
+                            </ul> 
+                        </Row>
+                </Col> 
+            </Row>
+            <Row  align="top" justify="center"> 
+                <Col span={8}>
+                        <Row> April 2018 - February 2019 </Row>
+                        <Row> Amida </Row>
+                        <Row> Washington DC </Row>
+                </Col> 
+                <Col span={16}> 
+                        <Row > <h3>UI/UX Developer</h3> </Row>
+                        <Row > Developed client applications with React. Conduct user interviews, user testing, and analytics for developmental work on VA Software. Develop front-end solutions for web applications. Producing wireframes, mockups, prototypes, and front-end code.
+                        </Row>
+                        <Row > 
+                            <ul>
+                                <li> Next.js, React, Svelte, Redux, eslint, Jest, Cypress, Playwright, webpack, Parcel, TypeScript, JavaScript </li>
+                                <li>Node.js, Express, ElasticSearch, Kibana, Spark, Java/Scala/Kotlin, Rust, Docker, Kubernetes, saltstack</li>
+                                <li>AWS, EC2, Mongo, PostgresSQL, Lambda, ECS, EMR, Terraform, GitHub, BitBucket, Jenkins, blockchain</li>
+                                <li>Jira, Confluence, ZenHub, Scaled Agile, Kanban, Microservices, Microfrontends, InnerSource</li>
+                            </ul> 
+                        </Row>
+                </Col> 
+            </Row>
+            <Row  align="top" justify="center"> 
+                <Col span={8}>
+                        <Row> January 2016 - March 2018 </Row>
+                        <Row> TRI </Row>
+                        <Row> Maryland </Row>
+                </Col> 
+                <Col span={16}> 
+                        <Row > <h3>Front End Developer</h3> </Row>
+                        <Row > Create, Edit, and/or Delete HTML and other files then publish changes to public website(s). Managing government webpages to provide 508 compliant and user-friendly experiences. Responsible for creating and delivering server-side code that follows company standards and procedures. Writing code and scripts to assist our database developers, testers, and senior developers.</Row>
+                        <Row > 
+                            <ul>
+                                <li> Next.js, React, Svelte, Redux, eslint, Jest, Cypress, Playwright, webpack, Parcel, TypeScript, JavaScript </li>
+                                <li>Node.js, Express, ElasticSearch, Kibana, Spark, Java/Scala/Kotlin, Rust, Docker, Kubernetes, saltstack</li>
+                                <li>AWS, EC2, Mongo, PostgresSQL, Lambda, ECS, EMR, Terraform, GitHub, BitBucket, Jenkins, blockchain</li>
+                                <li>Jira, Confluence, ZenHub, Scaled Agile, Kanban, Microservices, Microfrontends, InnerSource</li>
+                            </ul> 
+                        </Row>
+                </Col> 
+            </Row>
+            <Row  align="top" justify="center"> 
+                <Col span={8}>
+                        <Row> January 2014 - January 2016 </Row>
+                        <Row> The INCLab </Row>
+                        <Row> Tysons Corner, VA </Row>
+                </Col> 
+                <Col span={16}> 
+                        <Row > <h3>Senior Front End Developer</h3> </Row>
+                        <Row > Worked directly with clients to facilitate efficient design to development workflow, managed client expectations. Responsible for the creation of all visual assets; Marketing materials, animations, research, front-end code, data visualizations, wireframes, prototypes, design guides, and illustrations. Designed company website. https://www.theinclab.com </Row>
+                        <Row > 
+                            <ul>
+                                <li> Next.js, React, Svelte, Redux, eslint, Jest, Cypress, Playwright, webpack, Parcel, TypeScript, JavaScript </li>
+                                <li>Node.js, Express, ElasticSearch, Kibana, Spark, Java/Scala/Kotlin, Rust, Docker, Kubernetes, saltstack</li>
+                                <li>AWS, EC2, Mongo, PostgresSQL, Lambda, ECS, EMR, Terraform, GitHub, BitBucket, Jenkins, blockchain</li>
+                                <li>Jira, Confluence, ZenHub, Scaled Agile, Kanban, Microservices, Microfrontends, InnerSource</li>
+                            </ul> 
+                        </Row>
+                </Col> 
+            </Row>
+            <Row  align="top"> 
+                <Col span={8}>
+                    <h2> Education </h2>
+                </Col> 
+                <Col> 
+                    <Row><h2> Bachelors of Fine-Arts </h2></Row>
+                    <Row><h3> Virginia Commonwealth University, Richmond, VA </h3></Row>
+                    <Row><h3> 2009 - 2013 </h3></Row>
+                </Col> 
+            </Row>
 
-                <h2>Amida, Washington DC — UI/UX Developer </h2>
-                <h3><i>Mar 2019 - present</i></h3>
-                <p> Developed client applications with React. Conduct user interviews, user testing,
-and analytics for developmental work on VA Software. Develop front-end
-solutions for web applications. Producing wireframes, mockups, prototypes, and
-front-end code.</p>
-
-                <h2>TRI, Maryland— Front End Developer </h2>
-                <h3><i>January 2016 - March 2018</i></h3>
-                <p>Create, Edit, and/or Delete HTML and other files then publish changes to public
-website(s). Managing government webpages to provide 508 compliant and
-user-friendly experiences. Responsible for creating and delivering server-side
-code that follows company standards and procedures. Writing code and scripts to
-assist our database developers, testers, and senior developers.</p>
-
-                <h2> The INC Lab, Tysons, VA— 2D/3D Designer</h2>
-                <h3><i>January 2016 - March 2018</i></h3>
-                <p> Worked directly with clients to facilitate efficient design to development
-workflow, managed client expectations. Responsible for the creation of all visual
-assets; Marketing materials, animations, research, front-end code, data
-visualizations, wireframes, prototypes, design guides, and illustrations. Designed
-company website. https://www.theinclab.com</p>
-            </section>
             <h1>Skills</h1>
             <section>
-                <p> React, Node, Redux,
-Typescript, Javascript,
-Cypress, React Testing
-Library, Next.js
-CSS, SCSS, prettier, ESLint
-AWS, Docker, postman,
-Yarn, NPM, MySQL
-Confluence, Jira, GIT,
-GitLab, Jenkins
-Agile, Customer Relations,
-SCRUM Master
-Figma, Photoshop, Sketch,
-InVision, Adobe XD,
-Sharepoint</p>
+                <p> React, Node, Redux, Typescript, Javascript, Cypress, React Testing Library, Next.js CSS, SCSS, prettier, ESLint AWS, Docker, postman, Yarn, NPM, MySQL Confluence, Jira, GIT, GitLab, Jenkins Agile, Customer Relations, SCRUM Master Figma, Photoshop, Sketch, InVision, Adobe XD, Sharepoint</p>
             </section>
+
             <h1>Education</h1>
             <section>
-                <h2>Virginia Commonwealth University, Richmond, VA— Bachelors of
-Fine-Arts </h2>
-                <p>2009 - 2013 </p>
+                <h2>—  </h2>
+                <p> </p>
             </section>
-    </div>
+        </div>
+    </>
 }
